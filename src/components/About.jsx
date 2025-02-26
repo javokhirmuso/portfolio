a545587e0a77e98b73b7e1d19dd2ffc3155562e4
Javokhir Musaev <50003670+javokhirmuso@users.noreply.github.com>
2025-02-26 16:08:39 +0900

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,54 +61,7 @@ const About = () => {
 						focuses on deep learning applications in computer vision,
 						particularly in medical imaging and agricultural disease detection.
 					</motion.p>
-{/* 					<motion.p
-						variants={fadeIn('', '', 0.2, 1)}
-						className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-					>
-						During my doctoral studies, I co-authored a paper
-						proposing an improved convolutional neural network ensemble model
-						for medical image classification. This work introduced an enhanced
-						ensemble approach to improve the accuracy of medical image
-						classification by leveraging high-resolution image representations.
-					</motion.p> */}
-{/* 					<motion.p
-						variants={fadeIn('', '', 0.3, 1)}
-						className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-					>
-						In his postdoctoral research, Dr. Musaev co-developed the MICL-UNet
-						model, detailed in a publication that has garnered significant
-						attention. This model integrates multi-input data and cross-layer
-						connections to effectively classify agricultural diseases,
-						demonstrating the versatility of deep learning techniques in diverse
-						domains.
-					</motion.p>
-				</div>
-				<div className='p-0 lg:p-10 invisible h-0 w-0 overflow-hidden lg:visible lg:w-full lg:h-full'>
-					<img className='h-full w-full rounded-lg object-cover' src={avatar} />
-				</div>
-			</motion.div>
 
-			<motion.p
-				variants={fadeIn('', '', 0.4, 1)}
-				className='mt-4 text-secondary text-[17px]  leading-[30px]'
-			>
-				Dr. Musaev's contributions to the field are further evidenced by his
-				publication on hybrid convolutional network fusion, which explores
-				dual-pathway learning strategies to improve medical image classification
-				performance.
-			</motion.p>
-			<motion.p
-				variants={fadeIn('', '', 0.5, 1)}
-				className='mt-4 text-secondary text-[17px]  leading-[30px]'
-			>
-				Collectively, Dr. Musaev's research endeavors have significantly
-				advanced deep learning methodologies in computer vision, particularly in
-				the contexts of medical imaging and agricultural applications.
-			</motion.p>
-
-			<div className='py-10 lg:hidden'>
-				<img className='h-full w-full rounded-lg object-cover' src={avatar} />
-			</div> */}
 
 			<div className='mt-20 flex flex-wrap gap-10'>
 				{services.map((service, index) => (
