ad2d3c7656ffdb18b5f0e16255c6aa1979a72205
Javokhir Musaev <50003670+javokhirmuso@users.noreply.github.com>
2025-02-26 16:11:45 +0900

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,9 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
-import { avatar, phone } from '../assets';
 
 const ServiceCard = ({ index, title, icon, url }) => (
-	<Tilt className='xs:w-[250px] w-full'>
+	<Tilt className='xs:w-[250px] w-full' options={{ max: 45, scale: 1, speed: 450 }}>
 		<motion.div
 			variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
 			className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -18,22 +17,13 @@ const ServiceCard = ({ index, title, icon, url }) => (
 				href={url}
 				target='_blank'
 				rel='noopener noreferrer'
-				options={{
-					max: 45,
-					scale: 1,
-					speed: 450,
-				}}
 				className='bg-tertiary rounded-[20px] p-5 min-h-[280px] flex justify-evenly items-center flex-col'
 			>
 				<div className='w-16 h-16'>
-					<img
-						src={icon}
-						alt='web-development'
-						className=' object-cover h-full w-full'
-					/>
+					<img src={icon} alt={title} className='object-cover h-full w-full' />
 				</div>
 
-				<h3 className='text-white break-all flex flexc text-[20px] font-bold text-center'>
+				<h3 className='text-white break-all text-[20px] font-bold text-center'>
 					{title}
 				</h3>
 			</a>
@@ -49,25 +39,21 @@ const About = () => {
 				<h2 className={styles.sectionHeadText}>Overview.</h2>
 			</motion.div>
 
-			<motion.div className='grid grid-cols-1 lg:grid-cols-2'>
-				<div>
-					<motion.p
-						variants={fadeIn('', '', 0.1, 1)}
-						className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-					>
-						I am a postdoctoral researcher in the Department
-						of Computer Engineering at Yeungnam University, South Korea, where
-						I earned my Ph.D. in Computer Engineering in 2024. My research
-						focuses on deep learning applications in computer vision,
-						particularly in medical imaging and agricultural disease detection.
-					</motion.p>
+			<motion.p
+				variants={fadeIn('', '', 0.1, 1)}
+				className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+			>
+				I am a postdoctoral researcher in the Department of Computer Engineering at 
+				Yeungnam University, South Korea, where I earned my Ph.D. in Computer Engineering 
+				in 2024. My research focuses on deep learning applications in computer vision, 
+				particularly in medical imaging and agricultural disease detection.
+			</motion.p>
 
-
-			<div className='mt-20 flex flex-wrap gap-10'>
+			<motion.div className='mt-20 flex flex-wrap gap-10'>
 				{services.map((service, index) => (
 					<ServiceCard key={service.title} index={index} {...service} />
 				))}
-			</div>
+			</motion.div>
 		</>
 	);
 };
